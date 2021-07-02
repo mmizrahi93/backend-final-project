8e5b99d4a520f164d77ca6fccb9b8aaf50a5cada
Matan Mizrahi <matanmizrahi@Matans-MacBook-Air.local>
2021-07-02 14:53:08 -0400

    heroku up and running

--- models.py
+++ models.py
@@ -10,8 +10,7 @@ if 'ON_HEROKU' in os.environ:
     DATABASE = PostgresqlDatabase('dern94jbssvdkh', user='zvzvxyydfvrhvm', password='3a2adb4e7583a477af869d4880b2c803fc32e414b489ce6ff1ebb4bc187d8a43',
     host='ec2-54-147-93-73.compute-1.amazonaws.com', port=5432)
 else:
-    DATABASE = PostgresqlDatabase('dern94jbssvdkh', user='zvzvxyydfvrhvm', password='3a2adb4e7583a477af869d4880b2c803fc32e414b489ce6ff1ebb4bc187d8a43',
-    host='ec2-54-147-93-73.compute-1.amazonaws.com', port=5432)
+    DATABASE = PostgresqlDatabase('shows')
 
 class Show(Model):
     name = CharField(unique=True)
